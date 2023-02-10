ba8ec2e3710d2b63b937826c3419c037ebcf610c
Ghornon <szymon198198@gmail.com>
2023-02-10 12:27:52 +0100

    Add NotFound View

--- eBilety/Models/Cinema.cs
+++ eBilety/Models/Cinema.cs
@@ -9,7 +9,7 @@ namespace eBilety.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
 
     }
 }
