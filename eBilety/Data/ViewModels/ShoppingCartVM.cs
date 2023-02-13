f3d0a52be786fd80397eda5de1dbc114430a9a3f
Ghornon <szymon198198@gmail.com>
2023-02-13 19:20:30 +0100

    Add cart controller

---
+++ eBilety/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eBilety.Data.Cart;
+
+namespace eBilety.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
